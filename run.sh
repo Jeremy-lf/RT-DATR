9d5fd067d876e7d99e2391bf1bcfe470b25db222
Jeremy-lf <2637649812@qq.com>
2025-05-16 15:29:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # rtdetr-r34 cityscapes_to_foggycity
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export FLAGS_START_PORT=35100
-nohup /root/paddlejob/workspace/env_run/lvfeng/pd2.3_cuda11_py3.7/bin/python3.7 \
+nohup python3.7 \
 -m paddle.distributed.launch --gpus=0,1,2,3 --log_dir=log_base tools/train.py \
 -c configs/domain_adaption/da_r50_rtdetr_backbone_encoder_instance_dn_cmt_city2foggycity.yml \
 --eval \
